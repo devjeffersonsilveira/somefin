2f82b02acc1f0fe67e7440bad837b025cb274c17
jefferson <jefferson-silveira@univille.br>
2017-05-19 17:21:48 -0300

    ss

--- rest/server.js
+++ rest/server.js
@@ -1,13 +1,22 @@
+var conn = require('./conn');
 var express = require('express');
+potato = require('./routes/potato');
+carrot = require('./routes/carrot');
 var restapi = express();
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('rest.db');
 
-db.serialize(function() {
-  var command = "CREATE TABLE IF NOT EXISTS potato ( "
-  command += "id INTEGER IDENTITY  constraint pk_potato primary key, "
-  command += "des TEXT "
-  command += ");"
-    db.run(command);
-  //  db.run("INSERT INTO counts (key, value) VALUES (?, ?)", "counter", 0);
-});
+// app.configure(function () {
+//     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+//     app.use(express.bodyParser());
+// });
+
+restapi.post('/createPotatos', potato.createPotatos);
+restapi.get("getPotatos", potato.getPotatos);
+//restapi.get("getPotato/:id", potato.getPotato);
+restapi.post('/createCarrots', carrot.createCarrots);
+restapi.get("getCarrots", carrot.getCarrots);
+//restapi.get("getPotato/:id", carrot.getCarrot);
+
+
+restapi.listen(3000);
+
+console.log("running in http://localhost:3000/");
