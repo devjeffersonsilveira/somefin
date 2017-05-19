58638d03457398a5628fe1a0c04e959ef17a7c0b
jefferson <jefferson-silveira@univille.br>
2017-05-19 14:53:58 -0300

    agora show!

---
+++ nodecellar/server.js
@@ -0,0 +1,33 @@
+/*
+var express = require('express');
+
+var app = express();
+
+app.get('/wines', function(req, res) {
+    res.send([{name:'wine1'}, {name:'wine2'}]);
+});
+app.get('/wines/:id', function(req, res) {
+    res.send({id:req.params.id, name: "The Name", description: "description"});
+});
+
+app.listen(3000);
+console.log('Listening on port 3000...');
+*/
+var express = require('express'),
+    wine = require('./routes/wines');
+
+var app = express();
+
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
+app.get('/wines', wine.findAll);
+app.get('/wines/:id', wine.findById);
+app.post('/wines', wine.addWine);
+app.put('/wines/:id', wine.updateWine);
+app.delete('/wines/:id', wine.deleteWine);
+
+app.listen(3000);
+console.log('Listening on port 3000...');
