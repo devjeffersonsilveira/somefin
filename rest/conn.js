35adffe1dbc7dbe709c030152c1f5a3fd262e87a
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2017-05-20 11:46:39 -0300

    fim

--- rest/conn.js
+++ rest/conn.js
@@ -1,19 +1,18 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('rest.db');
-//var sql = require('./dbSchema')
-
-db.serialize(function() {
-  var sql = "CREATE TABLE IF NOT EXISTS potato ( ";
-  sql += "idPotato INTEGER constraint pk_potato primary key, ";
-  sql += "desPotato TEXT ";
-  sql += "); ";
-  db.run(sql);
-  sql = "CREATE TABLE IF NOT EXISTS carrot ( ";
-  sql += "idCarrot INTEGER constraint pk_carrot primary key, ";
-  sql += "idPotato INTEGER, ";
-  sql += "desCarrot TEXT,";
-  sql += "CONSTRAINT fk_potatoCarrot FOREIGN KEY (idPotato) REFERENCES potato(idPotato)";
-  sql += "); ";
-  db.run(sql);
-})
+var fs = require("fs");
+fs.readFile("dbSchema.sql", function(erro, data) {
+  if (erro) {
+    console.error(erro);
+  } else {
+    console.log(data.toString());
+    db.serialize(function() {
+      db.exec(data.toString(), function(err, row){
+        if (err) {
+          console.error(err);
+        }
+      });
+    });
+  }
+});
 module.exports = db;
--- rest/dbSchema.sql
+++ rest/dbSchema.sql
@@ -1,9 +1,10 @@
-CREATE TABLE IF NOT EXISTS potato ( 
-id INTEGER IDENTITY  constraint pk_potato primary key, 
-des TEXT 
-); 
-CREATE TABLE IF NOT EXISTS carrot ( 
-id INTEGER IDENTITY  constraint pk_carrot primary key, 
-des TEXT,
-idPotato int constrait fk_potatoCarrot foreign key reference (potato.id)
-); 
+CREATE TABLE IF NOT EXISTS potato (
+  idPotato INTEGER constraint pk_potato primary key,
+  desPotato TEXT
+);
+CREATE TABLE IF NOT EXISTS carrot (
+  idCarrot INTEGER constraint pk_carrot primary key,
+  idPotato INTEGER,
+  desCarrot TEXT,
+  CONSTRAINT fk_potatoCarrot FOREIGN KEY (idPotato) REFERENCES potato(idPotato)
+);
